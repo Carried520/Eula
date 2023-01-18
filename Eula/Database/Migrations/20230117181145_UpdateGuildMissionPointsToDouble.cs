8ec4e3a53d426cce6b9a1a89e9ed6b3b0925e319
47chromosomes <ostrowski.cz@gmail.com>
2023-01-18 20:52:04 +0100

    Exclude

---
+++ Eula/Database/Migrations/20230117181145_UpdateGuildMissionPointsToDouble.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Eula.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateGuildMissionPointsToDouble : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Points",
+                table: "GuildMissionDatas",
+                type: "double precision",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Points",
+                table: "GuildMissionDatas",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+        }
+    }
+}
